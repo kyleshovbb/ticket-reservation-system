3af929d259a698125e1ffad237fb3604352345c9
Vladimir Kuleshov <kyleshovbb@gmail.com>
2020-01-17 18:38:18 +0300

    create-autocomplete-field

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,31 @@ import { NgModule, ModuleWithProviders } from "@angular/core";
 import { AuthGuard } from "./guards/auth.guard";
 import { AuthService } from "./services/auth.service";
 import { UserService } from "./services/user.service";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+
+import { FakeBackendInterceptor } from "./fake-backend/fake-backend.interceptor";
+import { AirportsFinderInterceptor } from "./interceptors/airports-finder.interceptor";
 
 @NgModule()
 export class CoreModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: CoreModule,
-      providers: [UserService, AuthService, AuthGuard]
+      providers: [
+        UserService,
+        AuthService,
+        AuthGuard,
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: FakeBackendInterceptor,
+          multi: true
+        },
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: AirportsFinderInterceptor,
+          multi: true
+        }
+      ]
     };
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  airportsFinderApi: "https://cometari-airportsfinder-v1.p.rapidapi.com/api/"
 };
 
 /*
